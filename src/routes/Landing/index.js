15d1f8de3cb5c76abfd2a4d72d9c4a49b27935e9
Kasper Mikiewicz <IderedPL@gmail.com>
2016-09-02 20:56:23 +0200

    refactor: Import React.Component

--- src/routes/Landing/index.js
+++ src/routes/Landing/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default class Landing extends React.Component {
+export default class Landing extends Component {
   render() {
     return (
       <div>Hello</div>
--- src/routes/NotFound/index.js
+++ src/routes/NotFound/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default class NotFound extends React.Component {
+export default class NotFound extends Component {
   render() {
     return (
       <div>Page was not found.</div>
