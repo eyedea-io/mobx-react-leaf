4b1eeb4ddd82f947d4b3ab354f70bfbbcc4b1f24
Kasper Mikiewicz <IderedPL@gmail.com>
2016-09-02 22:59:49 +0200

    fix: Add store connect

--- src/routes/Landing/index.js
+++ src/routes/Landing/index.js
@@ -1,6 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import { connect, observer } from 'utils';
 
+@observer
+@connect(({ userStore, uiStore }) => ({
+  language: uiStore.language,
+  setLanguage: uiStore.setLanguage,
+  helloMessage: userStore.helloMessage,
+}))
 export default class Landing extends Component {
   static propTypes = {
     language: PropTypes.string,
