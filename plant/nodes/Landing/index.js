2c8c1fd66feae86011419ba00d62700c6219557c
Kasper Mikiewicz <IderedPL@gmail.com>
2016-09-06 00:08:59 +0200

    refactor: Landing node clean up

--- plant/nodes/Landing/index.js
+++ plant/nodes/Landing/index.js
@@ -1,31 +1,22 @@
 import React, { Component, PropTypes } from 'react';
 import { connect, observer, theme } from 'utils';
-import { Button } from 'leafs';
 
 @observer
-@connect(({ userRoot, uiRoot }) => ({
-  language: uiRoot.language,
-  setLanguage: uiRoot.setLanguage,
+@connect(({ userRoot }) => ({
   helloMessage: userRoot.helloMessage,
 }))
 @theme('Default')
 export default class Landing extends Component {
   static propTypes = {
-    language: PropTypes.string,
-    setLanguage: PropTypes.func,
     helloMessage: PropTypes.string,
   }
 
   render() {
-    const { helloMessage, setLanguage, language } = this.props;
+    const { helloMessage } = this.props;
 
     return (
       <div>
-        <p>{helloMessage}</p>
-        <Button
-          className="btn--primary"
-          onClick={() => setLanguage(language === 'en' ? 'pl' : 'en')}
-        >Toggle language</Button>
+        <p>{helloMessage}!</p>
       </div>
     );
   }
