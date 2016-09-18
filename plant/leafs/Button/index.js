d973a7f94f5e25d4abf3996bbf156f6d91991a2b
Kasper Mikiewicz <ideredpl@gmail.com>
2016-09-18 21:42:41 +0200

    refactor: make Button leaf stateless component

--- plant/leafs/Button/index.js
+++ plant/leafs/Button/index.js
@@ -1,23 +1,17 @@
-import React, { Component, PropTypes } from 'react';
-import classNames from 'classnames/bind';
+import React, { PropTypes } from 'react';
 import styles from './styles.scss';
 
-const cn = classNames.bind(styles);
+const cn = require('classnames/bind').bind(styles);
 
-export default class Button extends Component {
-  static propTypes = {
-    children: PropTypes.node,
-    className: PropTypes.string,
-  }
+const Button = ({ children, className, ...rest }) => (
+  <button {...rest} className={cn('btn', className)}>
+    {children}
+  </button>
+);
 
-  render() {
-    return (
-      <button
-        {...this.props}
-        className={cn('btn', this.props.className)}
-      >
-        {this.props.children}
-      </button>
-    );
-  }
-}
+Button.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+};
+
+export default Button;
