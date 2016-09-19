ec62c05ae2abb7c66db3916fa67108212a557581
Kasper Mikiewicz <ideredpl@gmail.com>
2016-09-19 19:59:47 +0200

    style: 404 page basic style

--- plant/nodes/NotFound/index.js
+++ plant/nodes/NotFound/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Link } from 'leafs';
+import styles from './styles.scss';
+
+const cn = require('classnames/bind').bind(styles);
 
 export default () => (
-  <div>Page was not found.</div>
+  <div className={cn('NotFound')}>
+    <div>
+      <h1 className={cn('NotFound__title')}>Page was not found.</h1>
+      <Link className={cn('NotFound__back')} to="/">Back to home</Link>
+    </div>
+  </div>
 );
