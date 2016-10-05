1a3cdfa794791817655b6f1a6b0bdd3dad475abf
Kasper Mikiewicz <ideredpl@gmail.com>
2016-10-05 18:10:06 +0200

    feat: dynamical bits/bytes export

--- src/bytes/index.js
+++ src/bytes/index.js
@@ -1,3 +1,14 @@
-export Layout from './Layout';
-export Header from './Header';
-export Wrapper from './Wrapper';
+// Find all bits in current directory
+const req = require.context('./', true, /^\.\/.*\/index.js/);
+
+// Export each bit
+req.keys().forEach((modulePath) => {
+  // Export name is based on bit directory
+  const moduleName = modulePath.match(/\.\/(.*)\//)[1];
+
+  // Export default module
+  module.exports[moduleName] = req(modulePath).default;
+});
+
+// Export vendor bits
+// ...
