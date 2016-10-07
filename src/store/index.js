45299fbd08c4f8a22544d83d651210bbe31998f7
Kasper Mikiewicz <ideredpl@gmail.com>
2016-10-07 15:04:32 +0200

    feat: add list of pending actions

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,8 @@ import app from './app';
 import user from './user';
 
 export default observable({
+  // List of pending actions/request
+  pending: asMap(new Set()),
   forms: asMap(),
   app,
   user,
